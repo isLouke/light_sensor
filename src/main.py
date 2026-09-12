b4a6bb8bfe8a1a3b5e6b8a3c2382aea3bd4e33ec
George Loukas <georgios.a.loukas@gmail.com>
2026-09-12 14:48:47 +0300

    refactor: Moved setup into class constructor. No need to be called from main separately.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 
 from hardware.TSL2591 import TSL2591
 
@@ -11,10 +10,6 @@ logger = logging.getLogger("main.py")
 
 def main():
     light_sensor = TSL2591()
-
-    if not light_sensor.setup():
-        logger.error("Light Sensor Setup Failed. Exiting...")
-        sys.exit()
 
     logger.info("Light Sensor Initialised. Main Loop Starts.")
     light_sensor.loop()
